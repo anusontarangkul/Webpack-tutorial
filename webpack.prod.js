1773d4e307e4a882304629e822b52580e2daa8a1
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-10-22 16:12:09 -0700

    update load

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,12 +3,41 @@ const path = require("path");
 const common = require("./webpack.common");
 const { merge } = require("webpack-merge");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = merge(common, {
     mode: "production",
     output: {
-        filename: "main.[contentHash].js",
+        filename: "[name].[contenthash].bundle.js",
         path: path.resolve(__dirname, "dist")
     },
-    plugins: [new CleanWebpackPlugin()]
+    optimization: {
+        minimizer: [
+            new OptimizeCssAssetsPlugin(), new TerserPlugin(), new HtmlWebpackPlugin({
+                template: "./src/template.html",
+                minify: {
+                    removeAttributeQuotes: true,
+                    collapseWhitespace: true,
+                    removeComments: true
+                }
+            })
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader, //3. Inject styles into DOM
+                    "css-loader", //2. Turns css into commonjs
+                    "sass-loader" //1. Turns sass into css
+                ]
+            },
+        ]
+    },
+
+    plugins: [new MiniCssExtractPlugin({ filename: "[name].[contenthash].css" }), new CleanWebpackPlugin()]
 });
